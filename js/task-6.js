7776e77f7068eee0c06ee8122d60fd92a9be9c3a
VitaliyKharytonov <vitalicaxaritonov@gmail.com>
2024-02-21 23:39:48 +0200

    add task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector("#controls").firstElementChild;
+const boxesEl = document.querySelector("#boxes");
+const createBtnEl = document.querySelector("[data-create]");
+const destroyBtnEl = document.querySelector("[data-destroy]");
+
+createBtnEl.addEventListener("click", (event) => {
+  if (inputEl.value >= 1 && inputEl.value <= 100) {
+    createBoxes(inputEl.value);
+  }
+  inputEl.value = "";
+});
+
+destroyBtnEl.addEventListener("click", (event) => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  boxesEl.innerHTML = "";
+  const box = [];
+  let boxsize = 30;
+  for (let i = 0; i < amount; i++) {
+    const div = `<div style="background-color: ${getRandomHexColor()};width: ${boxsize}px;height: ${boxsize}px"></div>`;
+    box.push(div);
+    boxsize += 10;
+  }
+  const boxEl = box.join("");
+  boxesEl.insertAdjacentHTML("beforeend", boxEl);
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
